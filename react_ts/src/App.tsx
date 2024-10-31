db7b05811359912faab69136f44b58f23e626d68
Sri2k <gandusrisai@gmail.com>
2024-10-31 18:39:06 +0530

    Code clean up

--- react_ts/src/App.tsx
+++ react_ts/src/App.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable */
 import "./App.css";
-import { Login } from "./components/Login";
-import UserContextProvider from "./components/UserContextProvider";
-import { UserProfile } from "./components/UserProfile";
-import { YoutubeForm } from "./components/YoutubeForm";
+import { Login } from "./components/hooks/user-context-usage/Login";
+import UserContextProvider from "./components/hooks/user-context-usage/UserContextProvider";
+import { UserProfile } from "./components/hooks/user-context-usage/UserProfile";
+
+import { YoutubeForm } from "./components/react-hook-form/YoutubeForm";
 
 function App() {
   return (
